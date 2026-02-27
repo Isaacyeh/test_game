89a54e39d43480862065e663be5a02c1292a5ca9
Isaac Yeh <101854219+IsaacYeh2009@users.noreply.github.com>
2026-02-27 15:39:44 -0600

    Update constant.js

--- script_files/constant.js
+++ script_files/constant.js
@@ -1,1 +1,11 @@
-
+//player constants
+export const PLAYER_RADIUS = 0.2;
+export const MOVE_SPEED = 0.05;
+export const FOV = Math.PI / 3;
+export const RAYS = canvas.width;
+export const JUMP_VELOCITY = 0.22;
+export const GRAVITY = 0.012;
+export const MAX_JUMP = 0.35;
+export const JUMP_SCALE = 200;
+export const MINIMAP_SCALE = 10;
+export const MINIMAP_PADDING = 10;
