6344e05ef31491b923c4a52ce552d87e2f363021
Isaac Yeh <101854219+IsaacYeh2009@users.noreply.github.com>
2026-02-27 15:39:59 -0600

    Update chat.js

--- script_files/chat.js
+++ script_files/chat.js
@@ -1,1 +1,42 @@
+import { isChatting } from "./player";
 
+export function setupChat(ws, chatInput, chat, sendBtn, username) {
+  function sendMessage() {
+    const msg = chatInput.value.trim();
+    if (!msg) return;
+    ws.send(JSON.stringify({ type: "chat", message: msg }));
+    chatInput.value = "";
+    chatInput.blur();
+  }
+
+  ws.addEventListener("open", () => {
+    ws.send(JSON.stringify({ type: "setName", name: username }));
+  });
+
+  sendBtn.addEventListener("click", sendMessage);
+
+  chatInput.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") sendMessage();
+  });
+  chatInput.addEventListener("focus", () => {
+    isChatting = true;
+  });
+  chatInput.addEventListener("blur", () => {
+    isChatting = false;
+  });
+  document.addEventListener("keydown", (e) => {
+    if (e.key === "/") {
+      chatInput.focus();
+    }
+  });
+  ws.addEventListener("message", (e) => {
+    const data = JSON.parse(e.data);
+
+    if (data.type === "chat") {
+      const div = document.createElement("div");
+      div.textContent = `${data.name}: ${data.message}`;
+      chat.appendChild(div);
+      chat.scrollTop = chat.scrollHeight;
+    }
+  });
+}
