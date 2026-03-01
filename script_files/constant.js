ed92c9c0732a3ea404bdb15fca5708f3f17b8161
Isaacyeh <101854141+Isaacyeh@users.noreply.github.com>
2026-02-28 21:37:17 -0600

    Update constant.js

--- script_files/constant.js
+++ script_files/constant.js
@@ -2,7 +2,7 @@
 export const PLAYER_RADIUS = 0.2;
 export const MOVE_SPEED = 0.05;
 export const FOV = Math.PI / 3;
-export const RAYS = canvas.width;
+//export const RAYS = canvas.width;
 export const JUMP_VELOCITY = 0.22;
 export const GRAVITY = 0.012;
 export const MAX_JUMP = 0.35;
