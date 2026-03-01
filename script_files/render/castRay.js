3ae10553553ad75161705f9ef20a6678f3ed447f
Isaacyeh <101854141+Isaacyeh@users.noreply.github.com>
2026-02-28 20:09:35 -0600

    Update castRay.js

--- script_files/render/castRay.js
+++ script_files/render/castRay.js
@@ -1,16 +1,21 @@
+import { getState } from "../player.js";
+import { isWall } from "../map.js";
+
 //rendering (walls)
 export function castRay(angle) {
-  const sin = Math.sin(angle),
-    cos = Math.cos(angle);
-  let d = 0,
-    prevX = player.x,
-    prevY = player.y;
+  const { player } = getState();
+  const sin = Math.sin(angle);
+  const cos = Math.cos(angle);
 
+  let d = 0;
+  let prevX = player.x;
+  let prevY = player.y;
   while (d < 20) {
     const x = player.x + cos * d;
     const y = player.y + sin * d;
-    if (isWall(x, y))
+    if (isWall(x, y)) {
       return { dist: d, vertical: Math.floor(x) !== Math.floor(prevX) };
+    }
     prevX = x;
     prevY = y;
     d += 0.02;
