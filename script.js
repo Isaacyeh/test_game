fbbf578199a983b9101f5cf141d193c9f918975e
Isaacyeh <101854141+Isaacyeh@users.noreply.github.com>
2026-03-25 15:41:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-import { initPlayer, update, getState, setMyId, setOthers } from "./script_files/player.js";
+import { initPlayer, update, getState, setMyId, setOthers, syncHealthFromServer } from "./script_files/player.js";
 import { setupChat } from "./script_files/chat.js";
 import { render } from "./script_files/render/render.js";
-
+ 
 const keys = {};
 window.addEventListener("keydown", (e) => {
   keys[e.key] = true;
@@ -9,33 +9,34 @@ window.addEventListener("keydown", (e) => {
 window.addEventListener("keyup", (e) => {
   keys[e.key] = false;
 });
-
+ 
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
-
-// chat elements
+ 
 const chat = document.getElementById("chat");
 const chatInput = document.getElementById("chatInput");
 const sendBtn = document.getElementById("sendBtn");
-// WebSocket
+ 
 const wsProtocol = location.protocol === "https:" ? "wss://" : "ws://";
 const ws = new WebSocket(wsProtocol + location.host);
-
+ 
 const { username } = getState();
 setupChat(ws, chatInput, chat, sendBtn, username);
 initPlayer(keys, ws);
-
-// NETWORK EVENTS
+ 
 ws.addEventListener("message", (e) => {
   const data = JSON.parse(e.data);
   if (data.type === "init") setMyId(data.id);
-  if (data.type === "players") setOthers(data.players);
+  if (data.type === "players") {
+    setOthers(data.players);
+    syncHealthFromServer(data.players);
+  }
 });
-
+ 
 function loop() {
   update();
   render(canvas, ctx);
   requestAnimationFrame(loop);
 }
-
+ 
 loop();
